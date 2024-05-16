44b251f68387c23cd9b72ac6b3bc4cfbbe681cf0
Roberto <bobo@di.unipmn.it>
2024-05-16 16:23:23 +0200

    Added italian translation

---
+++ lang/it/quizaccess_qrcode.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for the quizaccess_qrcode plugin.
+ *
+ * @package    quizaccess
+ * @subpackage qrcode
+ * @copyright  2021 Roberto Pinna
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Regola di accesso al quiz QRcode';
+$string['privacy:metadata'] = 'La regola di accesso al quiz QRcode non salva nessun dato personale.';
+$string['qrcodescan'] = 'Inserisci la password del quiz tramite QRcode';
+$string['qrcodestop'] = 'Chiudi lo scanner per QRcode';
+$string['useqrcode'] = 'Utilizza un QRcode per inserire la password del quiz';
+$string['useqrcode_help'] = 'Se abilitato, gli utenti potranno inserire la password del quiz utilizzando lo scanner per QRcode.'
